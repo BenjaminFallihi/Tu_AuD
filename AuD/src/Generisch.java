160fe995fa099b0dd33cb0a55ae9ac618692782e
benjamin <benjamin@enjamin.Speedport_W_724V_Typ_A_05011603_00_009>
2016-04-18 17:12:40 +0200

    add Class Genersich

---
+++ AuD/src/Generisch.java
@@ -0,0 +1,22 @@
+
+public class Generisch <T1, T2> {
+	
+	private T1 attribut1;
+	private T2 attribut2;
+	
+	public T1 getAttribut1() {
+		return attribut1;
+	}
+	public void setAttribut1(T1 attribut1) {
+		this.attribut1 = attribut1;
+	}
+	public T2 getAttribut2() {
+		return attribut2;
+	}
+	public void setAttribut2(T2 attribut2) {
+		this.attribut2 = attribut2;
+	}
+	
+	
+
+}
